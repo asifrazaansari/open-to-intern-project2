1778b0b6d831198b7ca07fa2f8be4f62a2b93bf5
asifrazaansari <asifraza0231@gmail.com>
2022-09-19 18:16:44 +0530

    backend integrated with frontend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const route = require('./routes/route')
 const mongoose = require('mongoose')
+const multer = require('multer')
 const app = express();
 
 app.use(express.json());
+app.use(multer().any())
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb+srv://Junaid619-DB:oS4jO8pwUnVaE0Fu@cluster0.4ufpuyj.mongodb.net/group65Database", {
@@ -18,6 +20,6 @@ app.use(function (req, res){
     return res.status(400).send({status: false, message: "Path not found"})
 })
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3001, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3001))
 })
